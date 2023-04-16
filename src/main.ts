939f5b707258f1be8deec1312fd871046f0b5cf5
TMK04 <floer.learner.01@gmail.com>
2023-04-16 20:49:55 +0800

    implement retrieve all envelopes route

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,29 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+type Envelope = {
+  category: string;
+  budget: number;
+};
+type Username = string;
+const username_to_envelopes: Record<Username, Envelope[]> = {
+  user1: [
+    { category: 'food', budget: 200 },
+    { category: 'clothes', budget: 100 },
+  ],
+};
+
+/**
+ * * Sample url: http://localhost:4000/user1/envelopes
+ */
+app.get('/:username/envelopes', (req, res) => {
+  const username = req.params.username;
+  const envelopes = username_to_envelopes[username];
+  if (!envelopes) {
+    res.status(404).send(`No envelopes found for ${username}`);
+    return;
+  }
+  res.json(envelopes);
+});
+
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
