0d940555fd2222ab879de1ae35a1f4c188e138c8
Timothy0ng <2101237b@student.tp.edu.sg>
2023-04-16 21:08:20 +0800

    get by category

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 type Envelope = {
-  category: string;
   budget: number;
 };
+type Category = string;
 type Username = string;
-const username_to_envelopes: Record<Username, Envelope[]> = {
-  user1: [
-    { category: 'food', budget: 200 },
-    { category: 'clothes', budget: 100 },
-  ],
+const username_to_envelopes: Record<Username, Record<Category, Envelope>> = {
+  user1: {
+    food: { budget: 200 },
+    clothes: { budget: 100 },
+  },
 };
 
 /**
@@ -27,6 +27,23 @@ app.get('/:username/envelopes', (req, res) => {
     return;
   }
   res.json(envelopes);
+});
+
+app.get('/:username/envelopes/:category', (req, res) => {
+  const username = req.params.username;
+  const category = req.params.category;
+  const envelopes = username_to_envelopes[username];
+  if (!envelopes) {
+    res.status(404).send(`No envelopes found for ${username}`);
+    return;
+  }
+
+  const envelope = envelopes[category];
+  if (!envelope) {
+    res.status(404).send(`Category ${category} not found`);
+    return;
+  }
+  res.json(envelope);
 });
 
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
